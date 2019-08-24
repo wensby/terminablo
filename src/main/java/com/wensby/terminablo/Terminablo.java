482ee765ba1b2f93a02db1fb7c4c2447e0d79b77
Lukas J. Wensby <wensby@gmail.com>
2019-08-24 21:53:49 +0900

    Extract class TerminalApplication

--- src/main/java/com/wensby/terminablo/Terminablo.java
+++ src/main/java/com/wensby/terminablo/Terminablo.java
@@ -1,9 +1,9 @@
 package com.wensby.terminablo;
 
+import com.wensby.terminalapp.TerminalApplication;
 import com.wensby.userinterface.BashCommandExecutor;
 import com.wensby.userinterface.linux.LinuxTerminal;
 import com.wensby.userinterface.linux.LinuxTerminalRenderCommandFactory;
-import org.apache.log4j.Logger;
 
 import static java.lang.System.in;
 import static java.lang.System.out;
@@ -14,17 +14,8 @@ public class Terminablo {
     var bashCommandExecutor = new BashCommandExecutor();
     var commandFactory = new LinuxTerminalRenderCommandFactory();
     var linuxTerminal = new LinuxTerminal(in, out, bashCommandExecutor, commandFactory);
-    linuxTerminal.setTerminalRaw();
-    linuxTerminal.showCursor(false);
-    try {
-      TerminabloFactory.createTerminabloGameLooper(linuxTerminal, commandFactory).run();
-    } catch (Throwable e) {
-      Logger.getLogger(Terminablo.class).fatal("Terminablo crashed.", e);
-    } finally {
-      linuxTerminal.moveCursor(0, 0);
-      linuxTerminal.setTerminalCooked();
-      linuxTerminal.showCursor(true);
-    }
+    var looper = TerminabloFactory.createTerminabloGameLooper(linuxTerminal, commandFactory);
+    new TerminalApplication(linuxTerminal, looper).start();
   }
 
 }
---
+++ src/main/java/com/wensby/terminalapp/TerminalApplication.java
@@ -0,0 +1,40 @@
+package com.wensby.terminalapp;
+
+import com.wensby.userinterface.linux.LinuxTerminal;
+import org.apache.log4j.Logger;
+
+
+public class TerminalApplication {
+
+  public static final Logger LOGGER = Logger.getLogger(TerminalApplication.class);
+
+  private final LinuxTerminal linuxTerminal;
+  private final GameLooper terminabloGameLooper;
+
+  public TerminalApplication(LinuxTerminal linuxTerminal, GameLooper terminabloGameLooper) {
+    this.linuxTerminal = linuxTerminal;
+    this.terminabloGameLooper = terminabloGameLooper;
+  }
+
+  public void start() {
+    try {
+      setUpTerminal();
+      terminabloGameLooper.run();
+    } catch (Throwable e) {
+      LOGGER.fatal("Terminal application crashed.", e);
+    } finally {
+      releaseTerminal();
+    }
+  }
+
+  private void setUpTerminal() {
+    linuxTerminal.setTerminalRaw();
+    linuxTerminal.showCursor(false);
+  }
+
+  private void releaseTerminal() {
+    linuxTerminal.moveCursor(0, 0);
+    linuxTerminal.setTerminalCooked();
+    linuxTerminal.showCursor(true);
+  }
+}
