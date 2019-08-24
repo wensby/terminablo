cef76cba5b2e99fa97b34bccf62d601e2357501e
Lukas J. Wensby <wensby@gmail.com>
2019-08-24 21:29:56 +0900

    Remove unused classes

--- src/main/java/com/wensby/terminablo/Renderable.java
+++
@@ -1,9 +0,0 @@
-package com.wensby.terminablo;
-
-import com.wensby.userinterface.InterfaceSize;
-import com.wensby.userinterface.TerminalLayer;
-
-public interface Renderable {
-
-  TerminalLayer render(InterfaceSize size);
-}
--- src/main/java/com/wensby/terminablo/Updatable.java
+++
@@ -1,9 +0,0 @@
-package com.wensby.terminablo;
-
-import com.wensby.userinterface.UserInput;
-import java.time.Duration;
-
-public interface Updatable {
-
-  void update(Duration elapsedTime, UserInput input);
-}
