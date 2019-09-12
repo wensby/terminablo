5a27b57b706b62422eae47af3159319fa1190d1e
Lukas J. Wensby <wensby@gmail.com>
2019-09-12 21:17:59 +0900

    Only try to render SimpleGrid children if present

--- src/main/java/com/wensby/terminablo/userinterface/reactive/SimpleGrid.java
+++ src/main/java/com/wensby/terminablo/userinterface/reactive/SimpleGrid.java
@@ -20,13 +20,15 @@ public class SimpleGrid implements Component {
 
   @Override
   public void render(TerminalLayer layer) {
-    var rows = (int)(children.size() / (float)columns + 0.5f);
-    var itemSize = InterfaceSize.of(layer.size().getWidth() / columns, layer.size().getHeight() / rows);
-    for (int i = 0; i < children.size(); i++) {
-      var row = i / columns;
-      var column = i % columns;
-      var topLeft = InterfaceLocation.at(column * itemSize.getWidth(), row * itemSize.getHeight());
-      children.get(i).render(layer.getSubsection(topLeft, itemSize));
+    if (children.size() > 0) {
+      var rows = (int) (children.size() / (float) columns + 0.5f);
+      var itemSize = InterfaceSize.of(layer.size().getWidth() / columns, layer.size().getHeight() / rows);
+      for (int i = 0; i < children.size(); i++) {
+        var row = i / columns;
+        var column = i % columns;
+        var topLeft = InterfaceLocation.at(column * itemSize.getWidth(), row * itemSize.getHeight());
+        children.get(i).render(layer.getSubsection(topLeft, itemSize));
+      }
     }
   }
 
