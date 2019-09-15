9bded216215589c6450bb2adee54d39b7de8e409
Lukas J. Wensby <wensby@gmail.com>
2019-09-15 18:31:17 +0900

    Put borders on MainMenuButton components

--- src/main/java/com/wensby/terminablo/scene/mainmenu/MainMenuButton.java
+++ src/main/java/com/wensby/terminablo/scene/mainmenu/MainMenuButton.java
@@ -1,9 +1,8 @@
 package com.wensby.terminablo.scene.mainmenu;
 
 import com.wensby.application.userinterface.TerminalCharacterFactory;
-import com.wensby.terminablo.userinterface.reactive.Button;
-import com.wensby.terminablo.userinterface.reactive.Component;
-import com.wensby.terminablo.userinterface.reactive.ReactiveComponent;
+import com.wensby.application.userinterface.TerminalLayerFactoryImpl;
+import com.wensby.terminablo.userinterface.reactive.*;
 
 
 public class MainMenuButton extends ReactiveComponent {
@@ -12,6 +11,8 @@ public class MainMenuButton extends ReactiveComponent {
   private final Runnable onClick;
   private final boolean focused;
   private final String label;
+  private final BorderStyle buttonBorderStyle;
+  private final BorderStyle selectedButtonBorderStyle;
 
   public MainMenuButton(TerminalCharacterFactory characterFactory, String label, Runnable onClick, boolean focused) {
     super();
@@ -19,15 +20,18 @@ public class MainMenuButton extends ReactiveComponent {
     this.label = label;
     this.onClick = onClick;
     this.focused = focused;
+    var borderStyleFactory = new BorderStyleFactory(new TerminalLayerFactoryImpl(), this.characterFactory);
+    buttonBorderStyle = borderStyleFactory.createButtonBorderStyle();
+    selectedButtonBorderStyle = borderStyleFactory.createSelectedButtonBorderStyle();
   }
 
   @Override
   public Component render() {
     if (focused) {
-      return new Button(characterFactory, String.format(">%s<", label), onClick);
+      return new Border(new Button(characterFactory, String.format(">%s<", label), onClick), buttonBorderStyle);
     }
     else {
-      return new Button(characterFactory, label, onClick);
+      return new Border(new Button(characterFactory, label, onClick), selectedButtonBorderStyle);
     }
   }
 }
